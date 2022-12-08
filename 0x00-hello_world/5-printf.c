d957015f855241fb4711d84295239d63c12320c5
Oluwapelumi <freshtunz58@gmail.com>
2022-12-08 15:51:57 -0800

    script to print to std output using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-/* script to print using printf */
-int main(void)
+/* script to print using printf */int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
