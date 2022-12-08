8f8ad3a9adde0c7738ebe705dd0c2a1c40c16960
Oluwapelumi <freshtunz58@gmail.com>
2022-12-08 15:20:32 -0800

    script to print to std output using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 
-/**
-* script to print to std output
-*/
+/*script to print to std output*/
 
-int main(void)
+int main(void) /* entry point into our code*/ 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
