9984875d965c16b89d8ea2fdec8cf8f450f65d91
Oluwapelumi <freshtunz58@gmail.com>
2022-12-08 22:35:26 -0800

    script to print to std output without puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,6 @@
+#include<stdlib.h>
+/* c script to print withot using  popular funcs */int main(void)/* entry point to oursourcecode */
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n' ");
+	return (1);
+}
