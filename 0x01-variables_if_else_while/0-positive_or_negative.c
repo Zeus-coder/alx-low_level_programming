d3776b4f311348ba921d5e1574d5bc6d451905a0
Oluwapelumi <freshtunz58@gmail.com>
2022-12-09 05:51:52 -0800

    script to print if a rand num is positve, zero or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,20 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+/* betty style doc for function main goes there */int main(void)/* betty style doc for function main goes there */ 
 {
 	int n;
 
 
 	srand(time(0));
-	n= rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n<0)
-		printf("%d is negative\n", n);
-	else if (n==0)
-		printf("%d is Zero\n", n);
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n < 0)
+		printf("%d is negative", n);
 	else
-		printf("%d is positive\n", n);
+		printf("%d is zero", n);
 
 	return (0);
 
