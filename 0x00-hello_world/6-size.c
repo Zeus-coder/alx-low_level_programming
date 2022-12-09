67390e169e8ff020d31f34f6816aec08d6679be6
Oluwapelumi <freshtunz58@gmail.com>
2022-12-08 16:40:35 -0800

    script to find the size of intrinsic type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 
-int i;
-char c;
-long int li;
-long long ll;
-float f;
-
 /* script to print size of */int main(void)/* entry point to our code */
 {
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-        printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(ll));
-	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+
+	int i;
+	char c;
+	long int li;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 
