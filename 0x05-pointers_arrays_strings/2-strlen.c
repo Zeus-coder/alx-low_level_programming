9c11dd581241bf4099bedcb432cb3a933320d0ff
Oluwapelumi <freshtunz58@gmail.com>
2022-12-20 09:45:54 +0000

    progra to print the len of a str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strlen - func that returns len of str
+*@l: len of string
+*@s: parameter of func
+*
+*a function that returns the len of str
+*/
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+
+	return (l);
+}
