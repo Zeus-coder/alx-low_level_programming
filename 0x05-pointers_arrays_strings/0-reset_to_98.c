72c0ca40b839cf2d5f3eb4b17ae42d45e2860de0
Oluwapelumi <freshtunz58@gmail.com>
2022-12-20 07:19:14 +0000

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*a function that takes a pointer to
+
+*an int as parameter and updates the
+
+*value it points to to 98.
+
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN
+#define MAIN
+extern int _putchar(int c);
+extern void reset_to_98(int *n);
+#endif
