c786197b7f38564f86d16024ca2a09a44dc6c1c0
Oluwapelumi <freshtunz58@gmail.com>
2022-12-20 07:42:23 +0000

    func that swap two numbers using pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*swap_int - function that swaps to numbers
+*@
+*@
+*
+*function that swaps to numbers
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+
+}
+
