86dafcf310ddf22db637bce59d46324ee44d3b50
Oluwapelumi <freshtunz58@gmail.com>
2022-12-21 07:57:53 +0000

    function that reverses a string.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,14 @@
 #define MAIN
 extern int _putchar(char c);
 extern void reset_to_98(int *n);
+extern int _strlen(char *s);
+extern void _puts(char *str);
+extern void print_rev(char *s);
+extern void rev_string(char *s);
+extern void puts2(char *str);
+extern void puts_half(char *str);
+extern void print_array(int *a, int n);
+extern char *_strcpy(char *dest, char *src);
+extern int _putchar(char c);
+
 #endif
