84b3c3d90209abd8ac1fa1e1ff8243f0bc445e46
Oluwapelumi <freshtunz58@gmail.com>
2023-01-02 22:38:55 +0000

    a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@ extern char *_memset(char *s, char b, unsigned int n);
 extern char *_memcpy(char *dest, char *src, unsigned int n);
 extern char *_strchr(char *s, char c);
 extern unsigned int _strspn(char *s, char *accept);
+extern char *_strpbrk(char *s, char *accept);
+
+
 
 #endif
