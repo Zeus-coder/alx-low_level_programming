bc60825037efb78d6c6384f4184fbdc7a8e43349
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 02:02:58 +0000

    a function that returns the factorial of a given number.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,6 @@ extern int _putchar(char c);
 extern void _puts_recursion(char *s);
 extern int _strlen_recursion(char *s);
 extern void _print_rev_recursion(char *s);
+extern int factorial(int n);
 
 #endif
