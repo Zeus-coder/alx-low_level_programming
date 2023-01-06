e689928086f627f0e3ff9f10e7f2aacb6d214039
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 20:41:09 +0000

    a program that prints all arguments it receives.

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,9 @@
 
 extern int main(int argc, char **argv);
 extern int main(int argc, __attribute__((unused)) char **argv);
-
+extern int main(int argc, char **argv);
+extern int main(int argc, char *argv[]);
+extern int main(int argc, char *argv[]);
 
 
 
