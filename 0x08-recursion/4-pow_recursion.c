3b25763cd808460383aa5f7b7221f8ea141274d4
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 02:14:11 +0000

    a function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+/**
+*_pow_recursion - function that returns x raised to the power of y.
+*@x: func arg1
+*@y: func arg2
+*
+*Return: x rasied to the power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+
+		return (x * _pow_recursion(x,y-1));
+	}
+
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,7 @@ extern void _puts_recursion(char *s);
 extern int _strlen_recursion(char *s);
 extern void _print_rev_recursion(char *s);
 extern int factorial(int n);
+extern int _pow_recursion(int x, int y);
+
 
 #endif
