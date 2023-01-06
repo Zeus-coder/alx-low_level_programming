05a948d5188b8c606a99c7496a8f317f4e43663d
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 02:47:12 +0000

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ extern int factorial(int n);
 extern int _pow_recursion(int x, int y);
 extern int _sqrt_recursion(int n);
 extern int is_prime_number(int n);
+extern int is_palindrome(char *s);
 
 
 
