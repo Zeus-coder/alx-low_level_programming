3d470b3cdfc91c9ba62e5f9d85beea64d825bed7
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 02:43:38 +0000

    a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,9 @@ extern void _print_rev_recursion(char *s);
 extern int factorial(int n);
 extern int _pow_recursion(int x, int y);
 extern int _sqrt_recursion(int n);
+extern int is_prime_number(int n);
+
+
 
 
 #endif
