3effcf4d1b92e060aff61aa2aec894fc8a7a06ff
Oluwapelumi <freshtunz58@gmail.com>
2023-01-06 02:40:40 +0000

    a function that returns the natural square root of a number.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,6 +6,7 @@ extern int _strlen_recursion(char *s);
 extern void _print_rev_recursion(char *s);
 extern int factorial(int n);
 extern int _pow_recursion(int x, int y);
+extern int _sqrt_recursion(int n);
 
 
 #endif
