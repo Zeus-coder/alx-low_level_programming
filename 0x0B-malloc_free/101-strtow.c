48bae5be7bf97770f37c5edad91e65adaec307c3
Oluwapelumi <freshtunz58@gmail.com>
2023-01-10 19:09:21 +0000

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
+ * **strtow - splits a string into words
+ *
+ * @str: string to split
+ * Return: pointer to array of strings, or NULL if fail
  */
 char **strtow(char *str)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	char **a;
+	int i, k, n, word_count = 0, word_len = 0, current_word = 0;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
+		if (*(str + i) != ' ')
+			word_count++;
+		while (*(str + i) != ' ' && *(str + i))
+			i++;
+		if (!*(str + i))
+			break;
 	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
+	if (word_count == 0)
 		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
+	a = malloc(sizeof(char *) * (word_count + 1));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0, k = i; current_word < word_count; i++, word_len = 0, k = i)
 	{
-		count = 0;
-		if (str[wordf] != ' ')
+		if (*(str + i) == ' ')
+			continue;
+		while (*(str + k) != ' ' && *(str + k++))
+			word_len++;
+		a[current_word] = malloc(sizeof(char) * word_len + 1);
+		if (!a[current_word])
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
+			while (current_word-- >= 0)
+				free(a[current_word]);
+			free(a);
+			return (NULL);
 		}
+		for (n = 0; i < k; i++, n++)
+			a[current_word][n] = *(str + i);
+		a[current_word++][n] = '\0';
 	}
-	p[j] = NULL;
-	return (p);
+	a[word_count] = NULL;
+	return (a);
 }
