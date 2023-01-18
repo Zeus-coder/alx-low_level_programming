dd651cc79b68edc2e51e8d3c98687cc4f4efa819
Oluwapelumi <freshtunz58@gmail.com>
2023-01-18 23:25:23 +0000

    a function that searches for an integer.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 extern int _putchar(char c);
 extern void print_name(char *name, void (*f)(char *));
 extern void array_iterator(int *array, size_t size, void (*action)(int));
-
+extern int int_index(int *array, int size, int (*cmp)(int));
 
 
 
